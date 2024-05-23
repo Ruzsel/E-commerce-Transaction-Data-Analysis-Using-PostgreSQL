e09de23b816a56661800b30659282db8f3ae69c3
Fairuz <150054552+Ruzsel@users.noreply.github.com>
2024-05-23 20:46:26 +0700

    Add files via upload

---
+++ Data/Copy csv file value into table.sql
@@ -0,0 +1,4 @@
+copy customer_detail FROM 'D:/1Work/Github/Ecommerce/Data/customer_detail.csv' DELIMITER ',' CSV HEADER;
+copy payment_detail FROM 'D:/1Work/Github/Ecommerce/Data/payment_detail.csv' DELIMITER ',' CSV HEADER;
+copy sku_detail FROM 'D:/1Work/Github/Ecommerce/Data/sku_detail.csv' DELIMITER ',' CSV HEADER;
+copy order_detail FROM 'D:/1Work/Github/Ecommerce/Data/order_detail.csv' DELIMITER ',' CSV HEADER;
